2e6a9403cf510afd480d8e73482cd68eeb310c93
David Bronfen <david.bronfen@gizra.com>
2017-05-23 21:00:28 +0300

    Header component (#9)
    * added header component
    
    * Added body reset styles
    
    * Code review fix, and added app-header to the main component
    
    * Added header wrapper and basic styles
    
    * Remove the title property from the app.component.html
    
    * Remove empty line
    
    * Remove test in order to pass the travis tests
    
    * Comment out test in order to pass the travis tests
    
    * data folder has been added to the project
    
    * changed annonymous user icon to svg, added some styles to the header
    
    * added buttons and buttonns style
    
    * Added background class to the body, removed the default unit test
    
    * Fix test, and remove old tests from app component
    
    * Removed the data folder from this PR
    
    * Changed logo icon to svg
    
    * Upgrade angular version and fix tests

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -2,9 +2,10 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  template: `
+    <app-header></app-header>
+  `,
+  styles: [``]
 })
 export class AppComponent {
-  title = 'app works!';
 }
--- client/src/polyfills.ts
+++ client/src/polyfills.ts
@@ -53,7 +53,7 @@ import 'core-js/es7/reflect';
 /***************************************************************************************************
  * Zone JS is required by Angular itself.
  */
-import 'zone.js/dist/zone';  // Included with Angular-CLI.
+import 'zone.js/dist/zone';  // Included with Angular CLI.
 
 
 
@@ -66,3 +66,7 @@ import 'zone.js/dist/zone';  // Included with Angular-CLI.
  * Needed for: All but Chrome, Firefox, Edge, IE11 and Safari 10
  */
 // import 'intl';  // Run `npm install --save intl`.
+/**
+ * Need to import at least one locale-data with intl.
+ */
+// import 'intl/locale-data/jsonp/en';
