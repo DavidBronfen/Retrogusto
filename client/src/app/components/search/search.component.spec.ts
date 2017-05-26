f03c3d26662adf29256dc333737e5f393474d012
David Bronfen <davidbronfen@gmail.com>
2017-05-26 23:34:15 +0300

    added the search input and some styles defined

--- client/src/app/components/search/search.component.spec.ts
+++ client/src/app/components/search/search.component.spec.ts
@@ -1,4 +1,5 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { MaterialModule } from '@angular/material';
 
 import { SearchComponent } from './search.component';
 
@@ -8,7 +9,8 @@ describe('SearchComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ SearchComponent ]
+      declarations: [ SearchComponent ],
+      imports: [ MaterialModule ]
     })
     .compileComponents();
   }));
