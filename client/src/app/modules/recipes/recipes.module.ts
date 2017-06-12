b8d187fdbbae0c39092045f6a5f03fbb23ece82b
David Bronfen <davidbronfen@gmail.com>
2017-06-12 20:37:05 +0300

    Added recipes component

--- client/src/app/modules/recipes/recipes.module.ts
+++ client/src/app/modules/recipes/recipes.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RecipesComponent } from './recipes.component';
 
 @NgModule({
   imports: [
     CommonModule
   ],
-  declarations: []
+  declarations: [
+    RecipesComponent
+  ]
 })
 export class RecipesModule { }
