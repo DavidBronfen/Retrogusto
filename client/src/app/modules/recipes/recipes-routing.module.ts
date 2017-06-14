7e643da88477943f4a1c6571e09f38e766419908
David Bronfen <david.bronfen@gizra.com>
2017-06-14 11:16:44 +0300

    Added missing semicoln

--- client/src/app/modules/recipes/recipes-routing.module.ts
+++ client/src/app/modules/recipes/recipes-routing.module.ts
@@ -5,7 +5,7 @@ import { RecipesComponent } from './recipes.component';
 
 const routes: Routes = [
   {path: '', component: RecipesComponent}
-]
+];
 
 @NgModule({
   imports: [ RouterModule.forRoot(routes) ],
