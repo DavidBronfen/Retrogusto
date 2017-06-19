f93021eb8ec482e4b6ef519cc79d03e07a4c3e0d
David Bronfen <davidbronfen@gmail.com>
2017-06-19 22:49:34 +0300

    Added category interface.

--- client/src/app/components/categories/categories.component.ts
+++ client/src/app/components/categories/categories.component.ts
@@ -1,11 +1,14 @@
 import { Component, OnInit } from '@angular/core';
 
+import { ICategory } from './category'
+
 @Component({
   selector: 'rg-categories',
   templateUrl: './categories.component.html',
   styleUrls: ['./categories.component.scss']
 })
 export class CategoriesComponent implements OnInit {
+  categories: ICategory[];
 
   constructor() { }
 
