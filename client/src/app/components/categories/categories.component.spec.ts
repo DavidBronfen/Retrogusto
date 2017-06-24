3fb9eeea980b4cfb7557ae17d80f5f5c45d09670
David Bronfen <davidbronfen@gmail.com>
2017-06-24 11:43:12 +0300

    NO ERRORS SCHEMA has been added to the category and categories component

--- client/src/app/components/categories/categories.component.spec.ts
+++ client/src/app/components/categories/categories.component.spec.ts
@@ -1,5 +1,5 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { NO_ERRORS_SCHEMA } from '@angular/core';
 import { CategoriesComponent } from './categories.component';
 
 describe('CategoriesComponent', () => {
@@ -8,7 +8,8 @@ describe('CategoriesComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ CategoriesComponent ]
+      declarations: [ CategoriesComponent ],
+      schemas: [ NO_ERRORS_SCHEMA ]
     })
     .compileComponents();
   }));
--- client/src/app/components/category/category.component.spec.ts
+++ client/src/app/components/category/category.component.spec.ts
@@ -1,5 +1,5 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { NO_ERRORS_SCHEMA } from '@angular/core';
 import { CategoryComponent } from './category.component';
 
 describe('CategoryComponent', () => {
@@ -8,7 +8,8 @@ describe('CategoryComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ CategoryComponent ]
+      declarations: [ CategoryComponent ],
+      schemas: [ NO_ERRORS_SCHEMA ]
     })
     .compileComponents();
   }));
