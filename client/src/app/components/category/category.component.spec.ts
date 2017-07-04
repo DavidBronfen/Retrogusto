851987db8c3446356db17af486d8d24cfd1d5550
David Bronfen <davidbronfen@gmail.com>
2017-07-04 20:25:18 +0300

    Added NO _ERROR_SCHEMAS to the test

--- client/src/app/components/category/category.component.spec.ts
+++ client/src/app/components/category/category.component.spec.ts
@@ -1,5 +1,6 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { CategoryComponent } from './category.component';
+import { NO_ERRORS_SCHEMA } from '@angular/core';
 
 describe('CategoryComponent', () => {
   let component: CategoryComponent;
@@ -8,6 +9,7 @@ describe('CategoryComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [ CategoryComponent ],
+      schemas: [ NO_ERRORS_SCHEMA ]
     })
     .compileComponents();
   }));
