3566311a1d59833ba6621c055235cb0000df4e68
David Bronfen <davidbronfen@gmail.com>
2017-07-08 11:16:09 +0300

    Fix test, added http mock to the service test, fix material warnning

--- client/src/app/components/categories/categories.component.spec.ts
+++ client/src/app/components/categories/categories.component.spec.ts
@@ -1,4 +1,5 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 
 import { ICategory } from './category';
 import { Observable } from 'rxjs/Observable';
@@ -6,15 +7,15 @@ import { Observable } from 'rxjs/Observable';
 import { CategoriesComponent } from './categories.component';
 import { CategoriesService } from '../../services/categories.service';
 
-const CATEGORIES_OBJECT: ICategory = {
+const CATEGORIES_OBJECT: ICategory[] = [{
   id: 1,
   name: 'ארוחת בוקר',
   image_path: 'data/categories/breakfast.png'
-};
+}];
 
 class MockCategory {
 
-  public getCategories(): Observable<ICategory> {
+  public getCategories(): Observable<ICategory[]> {
     return Observable.of(CATEGORIES_OBJECT);
   }
 }
@@ -27,8 +28,11 @@ describe('CategoriesComponent', () => {
     TestBed.configureTestingModule({
       declarations: [ CategoriesComponent ],
       providers: [
-      { provide: CategoriesService, useClass: MockCategory }
-    ]
+        { provide: CategoriesService, useClass: MockCategory }
+      ],
+      schemas: [
+        CUSTOM_ELEMENTS_SCHEMA
+      ]
     })
     .compileComponents();
   }));
--- client/src/app/services/categories.service.spec.ts
+++ client/src/app/services/categories.service.spec.ts
@@ -1,11 +1,25 @@
 import { TestBed, inject } from '@angular/core/testing';
+import { HttpModule, Http, BaseRequestOptions, Response, ResponseOptions } from '@angular/http';
+import { MockBackend } from '@angular/http/testing';
 
 import { CategoriesService } from './categories.service';
 
 describe('CategoriesService', () => {
   beforeEach(() => {
     TestBed.configureTestingModule({
-      providers: [CategoriesService]
+      imports: [ HttpModule ],
+      providers: [
+        CategoriesService,
+        {
+          provide: Http,
+          useFactory: (mockBackend, options) => {
+            return new Http(mockBackend, options);
+          },
+          deps: [ MockBackend, BaseRequestOptions ]
+        },
+        MockBackend,
+        BaseRequestOptions,
+      ]
     });
   });
 
