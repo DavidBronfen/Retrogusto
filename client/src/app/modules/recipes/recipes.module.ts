f7789d6ddaa49f5ef1cc0f811f4f9c8c892bafe8
David Bronfen <davidbronfen@gmail.com>
2017-08-02 20:08:38 +0300

    Recipes module imports.

--- client/src/app/modules/recipes/recipes.module.ts
+++ client/src/app/modules/recipes/recipes.module.ts
@@ -1,19 +1,28 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RecipesRoutingModule } from './recipes-routing.module';
 
 import { MaterialModule } from '@angular/material';
 
-import { RecipesRoutingModule } from './recipes-routing.module';
+import { StoreModule } from '@ngrx/store';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { EffectsModule } from '@ngrx/effects';
+import { reducer } from '../../reducers';
 
 import { RecipesComponent } from './recipes.component';
 import { RecipeComponent } from './components/recipe/recipe.component';
 import { RecipesService } from '../../services/recipes.service';
+
+import { RecipesEffects } from '../../effects/recipes';
 
 @NgModule({
   imports: [
     RecipesRoutingModule,
     CommonModule,
     MaterialModule,
+    StoreModule.provideStore(reducer),
+    StoreDevtoolsModule.instrumentOnlyWithExtension(),
+    EffectsModule.run(RecipesEffects),
   ],
   declarations: [
     RecipesComponent,
