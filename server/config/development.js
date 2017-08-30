eb415b61681e83e559f795aaf5e1962962774992
David Bronfen <david.bronfen@gizra.com>
2017-08-30 23:37:16 +0300

    Server configuration (#48)
    * Added server configuration, environments, logger and middleware.
    
    * Fix Errors.
    
    * Code review comments fix.

---
+++ server/config/development.js
@@ -0,0 +1,8 @@
+module.exports = {
+  // Enable logging for development.
+  logging: true,
+  seed: false, // Change to true when seed will be set.
+  db: {
+    url: 'mongodb://localhost/retrogusto',
+  }
+}
---
+++ server/config/production.js
@@ -0,0 +1,8 @@
+module.exports = {
+  // Disable logging for production.
+  logging: false,
+  seed: false,
+  db: {
+    url: 'mongodb//localhost/retrogusto-prod',
+  }
+}
