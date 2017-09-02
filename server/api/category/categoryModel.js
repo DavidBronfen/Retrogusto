e59e45d4ad616b813c17b35a77fbc98d2fb8c5c4
David Bronfen <davidbronfen@gmail.com>
2017-09-02 15:58:51 +0300

    Category model has been created.

---
+++ server/api/category/categoryModel.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+const CategorySchema = new Schema({
+  name_he: {
+    type: String;
+    required: true;
+  },
+  name_en: {
+    type: String;
+    required: true;
+  },
+  image_path: {
+    type: String;
+    required: true;
+  },
+});
+
+module.exports = mongoose.model('category', CategorySchema);
