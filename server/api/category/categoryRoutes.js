f51d45382fcc69475334b01919a431d4b1fbfc07
David Bronfen <davidbronfen@gmail.com>
2017-09-02 17:34:09 +0300

    Category routes has been added.

---
+++ server/api/category/categoryRoutes.js
@@ -0,0 +1,16 @@
+const router = require('express').Router();
+const logger = require('../../util/logger');
+const controller = require('./categoryController');
+
+router.param('id', controller.params);
+
+router.route('/')
+  .get(controller.get)
+  .post(controller.post)
+
+router.route('/:id')
+  .get(controller.getOne)
+  .put(controller.put)
+  .delete(controller.delete)
+
+module.exports = router;
