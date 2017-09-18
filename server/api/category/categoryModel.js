24f76f6c8f971b232b3ec6041be4b2d0f9ea1ce3
David Bronfen <davidbronfen@gmail.com>
2017-09-18 18:42:00 +0300

    Fix semicolon to coma.

--- server/api/category/categoryModel.js
+++ server/api/category/categoryModel.js
@@ -4,16 +4,16 @@ const Schema = mongoose.Schema;
 
 const CategorySchema = new Schema({
   name_he: {
-    type: String;
-    required: true;
+    type: String,
+    required: true,
   },
   name_en: {
-    type: String;
-    required: true;
+    type: String,
+    required: true,
   },
   image_path: {
-    type: String;
-    required: true;
+    type: String,
+    required: true,
   },
 });
 
