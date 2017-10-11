77e55b25cb6f5106edaaa8199c1044160d0cbd10
David Bronfen <davidb@netcraft.co.il>
2017-10-11 10:35:05 +0300

    Added first test to get all categories.

---
+++ server/api/category/category.spec.js
@@ -0,0 +1,21 @@
+process.env.NODE_ENV = 'testing';
+
+const chai = require('chai');
+const chaiHttp = require('chai-http');
+const server = require('../../server');
+
+const should = chai.should();
+
+chai.use(chaiHttp);
+
+describe('Test categories', () => {
+  it('Should GET all the categoties', (done) => {
+    chai.request(server)
+      .get('/api/categories')
+      .end((err, res) => {
+        res.should.have.status(200);
+        console.log(res);
+        done();
+      });
+  });
+});
