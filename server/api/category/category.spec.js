84ed3452fa92dbe8fce065c0edee0767273e7544
David Bronfen <davidbronfen@gmail.com>
2017-10-12 13:53:56 +0300

    Return the done function.

--- server/api/category/category.spec.js
+++ server/api/category/category.spec.js
@@ -9,12 +9,15 @@ const should = chai.should();
 chai.use(chaiHttp);
 
 describe('Test categories', () => {
-  it('Should GET all the categoties', () => {
+  it('Should GET all the categoties', (done) => {
     chai.request(server)
       .get('/api/categories')
       .end((err, res) => {
-        console.log('the result: ', res);
+        if (err) done(err);
+        console.log('the result: ', res.body);
         res.should.have.status(200);
+        res.body.should.be.a('array');
+        done();
       });
   });
 });
