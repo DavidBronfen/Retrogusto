03c0f978a76e5ef0795c3de24aa96fe1319e467a
David Bronfen <davidbronfen@gmail.com>
2017-10-17 11:01:26 +0300

    Fix lint and schema error

--- server/api/category/categoryModel.js
+++ server/api/category/categoryModel.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 
-const { Schema } = mongoose.Schema;
+/* eslint prefer-destructuring: ["error", {AssignmentExpression: {array: true}}] */
+const Schema = mongoose.Schema;
 
 const CategorySchema = new Schema({
   name_he: {
