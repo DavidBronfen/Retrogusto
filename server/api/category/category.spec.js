8bdba31faf2ba37d2f3830541356ee4907d5a3b6
David Bronfen <davidb@netcraft.co.il>
2017-10-17 16:42:09 +0300

    Added post test

--- server/api/category/category.spec.js
+++ server/api/category/category.spec.js
@@ -14,10 +14,26 @@ describe('Test categories', () => {
       .get('/api/categories')
       .end((err, res) => {
         if (err) done(err);
-        // console.log('the result: ', res.body);
+        console.log('the result: ', res.body);
         res.should.have.status(200);
         res.body.should.be.a('array');
         done();
+      });
+  });
+
+  it('Should be able to post new category', () => {
+    const newCategory = {
+      name_he: 'חמוצים',
+      name_en: 'Pickles',
+      image_path: 'path/to/image',
+    }
+
+    chai.request(server)
+      .post('/api/categories')
+      .send(newCategory)
+      .end((err, res) => {
+        console.log(res);
+        res.should.have.status(200);
       });
   });
 });
