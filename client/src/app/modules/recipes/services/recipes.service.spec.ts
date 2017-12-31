b70d7bfc35443837d6570464cb3ada5900612e52
David Bronfen <davidbronfen@gmail.com>
2017-12-31 20:17:33 +0200

    Change the recipe service test to contain 'HttpClient'.

--- client/src/app/modules/recipes/services/recipes.service.spec.ts
+++ client/src/app/modules/recipes/services/recipes.service.spec.ts
@@ -1,30 +1,33 @@
-import { TestBed, inject } from '@angular/core/testing';
-import { HttpClientModule } from '@angular/common/http';
-import { Http, BaseRequestOptions, Response, ResponseOptions } from '@angular/http';
-import { MockBackend } from '@angular/http/testing';
+import { TestBed, async, inject } from '@angular/core/testing';
+import { HttpClientModule, HttpClient } from '@angular/common/http';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
+
+// import { Http, BaseRequestOptions, Response, ResponseOptions } from '@angular/http';
+// import { MockBackend } from '@angular/http/testing';
 
 import { RecipesService } from './recipes.service';
 
 describe('RecipesService', () => {
+
   beforeEach(() => {
     TestBed.configureTestingModule({
-      imports: [ HttpClientModule ],
-      providers: [
-        RecipesService,
-        {
-          provide: Http,
-          useFactory: (mockBackend, options) => {
-            return new Http(mockBackend, options);
-          },
-          deps: [ MockBackend, BaseRequestOptions ]
-        },
-        MockBackend,
-        BaseRequestOptions,
-      ]
+      imports: [
+        HttpClientModule,
+        HttpClientTestingModule
+      ],
     });
   });
 
-  it('should be created', inject([RecipesService], (service: RecipesService) => {
-    expect(service).toBeTruthy();
-  }));
+  it('should issue a request',
+    async(
+      inject([HttpClient, HttpTestingController], (http: HttpClient, backend: HttpTestingController) => {
+        http.get('data/recipes.json').subscribe();
+
+        backend.expectOne({
+          url: 'data/recipes.json',
+          method: 'GET'
+        });
+      })
+    )
+  );
 });
