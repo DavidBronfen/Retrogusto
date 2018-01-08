a1213216b23b294f52e95c042aa42f1d1045a261
David Bronfen <davidbronfen@gmail.com>
2018-01-08 22:36:21 +0200

    Added login toggle function; Dispattch toggle login popup; Subscribe to login popup state.

--- client/src/app/components/header/header.component.ts
+++ client/src/app/components/header/header.component.ts
@@ -1,16 +1,30 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { Observable } from 'rxjs/Observable';
+import { Store } from '@ngrx/store';
 
 import { SearchComponent } from '../search/search.component';
+
+import * as loginAction from '../../actions/login';
+import * as fromRoot from '../../reducers';
+
 
 @Component({
     selector: 'rg-header',
     templateUrl: './header.component.html',
     styleUrls: ['./header.component.scss']
 })
-export class HeaderComponent implements OnInit {
+export class HeaderComponent {
+  public showLogin: boolean;
 
-    constructor() { }
+  constructor(
+    private store: Store<fromRoot.State>,
+  ) {
+    this.store.select(fromRoot.getLoginPopupState).subscribe(
+      loginPopupState => this.showLogin = loginPopupState;
+    );
+  }
 
-    ngOnInit() {
-    }
+  toggleLogin() {
+    this.store.dispatch(new loginAction.ToggleLoginPopupAction());
+  }
 }
