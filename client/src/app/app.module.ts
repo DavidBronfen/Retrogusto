037a9cb13acb89b3002601ddd2ee23fc9b01e811
David Bronfen <davidbronfen@gmail.com>
2018-01-09 10:52:14 +0200

    Merged with master.

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -28,7 +28,6 @@ import { HeaderComponent } from './components/header/header.component';
 import { SearchComponent } from './components/search/search.component';
 import { CategoriesComponent } from './components/categories/categories.component';
 import { CategoryComponent } from './components/category/category.component';
-import { LoginComponent } from './components/login/login.component';
 
 import { CategoriesService } from './services/categories.service';
 
@@ -43,7 +42,6 @@ import { environment } from '../environments/environment';
     SearchComponent,
     CategoriesComponent,
     CategoryComponent,
-    LoginComponent,
   ],
   imports: [
     BrowserModule,
@@ -62,9 +60,7 @@ import { environment } from '../environments/environment';
     ]),
     FlexLayoutModule,
   ],
-  providers: [
-    CategoriesService,
-  ],
+  providers: [ CategoriesService ],
   bootstrap: [ AppComponent ]
 })
 export class AppModule { }
