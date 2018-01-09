f03643d3ba079038a7f48ef6468ceb81c2ad944e
David Bronfen <davidbronfen@gmail.com>
2018-01-09 14:10:19 +0200

    Import map function.

--- client/src/app/services/categories.service.ts
+++ client/src/app/services/categories.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient, HttpResponse, HttpErrorResponse } from '@angular/common/http';
 import { Observable } from 'rxjs/Observable';
 import 'rxjs/add/operator/catch';
+import 'rxjs/add/operator/map';
 
 import { environment } from '../../environments/environment'
 
