766c42d5431fb5a759e3bf75e46468552e158dab
David Bronfen <davidbronfen@gmail.com>
2018-01-30 22:26:41 +0200

    Fix lint error.

--- client/src/app/components/header/header.component.ts
+++ client/src/app/components/header/header.component.ts
@@ -1,8 +1,7 @@
 import { Component } from '@angular/core';
 import { Observable } from 'rxjs/Observable';
 import { Store } from '@ngrx/store';
-import {MatDialog, MatDialogRef, MAT_DIALOG_DATA} from '@angular/material';
-
+import { MatDialog, MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 
 import { SearchComponent } from '../search/search.component';
 import { LoginComponent } from '../login/login.component';
@@ -17,26 +16,19 @@ import * as fromRoot from '../../reducers';
     styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent {
-  public showLogin: boolean;
 
   constructor(
     private store: Store<fromRoot.State>,
     public dialog: MatDialog,
-  ) {
-    // this.store.select(fromRoot.getLoginPopupState).subscribe(
-    //   loginPopupState => this.showLogin = loginPopupState
-    // );
-  }
+  ) { }
 
   toggleLogin() {
-    // this.store.dispatch(new loginAction.ToggleLoginPopupAction());
-    let dialogRef = this.dialog.open(LoginComponent, {
+    const dialogRef = this.dialog.open(LoginComponent, {
       width: '25vw',
       panelClass: 'login-dialog'
     });
 
     dialogRef.afterClosed().subscribe(result => {
-      console.warn(result);
       console.log('The dialog was closed');
     });
 
