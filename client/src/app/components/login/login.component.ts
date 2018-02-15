e76b38400ab3c1ba6e87239fb9475c51a5db815c
David Bronfen <davidbronfen@gmail.com>
2018-02-15 20:52:47 +0200

    Add login form validation and dispatch command to login user action.

--- client/src/app/components/login/login.component.ts
+++ client/src/app/components/login/login.component.ts
@@ -1,5 +1,11 @@
 import { Component, OnInit, Inject, ViewEncapsulation } from '@angular/core';
+import { FormGroup, FormControl, Validators, ValidatorFn, AbstractControl } from '@angular/forms';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material';
+import { Store } from '@ngrx/store';
+
+import * as fromRoot from '../../reducers';
+import { LoginUserAction } from '../../actions/login';
+import { ILogin } from '../../models/login';
 
 
 @Component({
@@ -7,19 +13,52 @@ import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material';
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss'],
   encapsulation: ViewEncapsulation.None
-
-
 })
-export class LoginComponent {
 
+export class LoginComponent implements OnInit {
+  loginForm: FormGroup;
 
   constructor(
+    @Inject(MAT_DIALOG_DATA) public data: any,
     public dialogRef: MatDialogRef<LoginComponent>,
-    @Inject(MAT_DIALOG_DATA) public data: any) {
+    private store: Store<fromRoot.State>,
+  ) {
+  }
+
+  ngOnInit() {
+    this.createForm();
+  }
+
+  public onSubmit() {
+    const loginInfo = {
+      email: this.loginForm.value.email,
+      password: this.loginForm.value.password,
+    } as ILogin;
+    this.store.dispatch(new LoginUserAction(loginInfo));
+  }
+
+  private createForm() {
+    this.loginForm = new FormGroup({
+      'email': new FormControl('', [
+        Validators.required,
+        // tslint:disable-next-line
+        this.patternValidator(/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/)
+      ]),
+      'password': new FormControl('', Validators.required),
+    });
+  }
+
+  private patternValidator(regexp: RegExp): ValidatorFn {
+    return (control: AbstractControl): { [key: string]: any } => {
+      const value = control.value;
+      if (value === '') {
+        return null;
+      }
+      return !regexp.test(value) ? { 'patternInvalid': { regexp } } : null;
+    };
   }
 
   onNoClick(): void {
     this.dialogRef.close();
   }
-
 }
