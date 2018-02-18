b1270837522c552a26741f732d89eae4c56b7d78
David Bronfen <davidbronfen@gmail.com>
2018-02-18 21:57:48 +0200

    Login service has been updated (will not work till server api will be created)

--- client/src/app/services/login.service.ts
+++ client/src/app/services/login.service.ts
@@ -1,8 +1,24 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs/Observable';
+
+import { ILogin, ILoginToken } from '../models/login';
+
+import { environment } from '../../environments/environment';
 
 @Injectable()
 export class LoginService {
 
-  constructor() { }
+  constructor(
+    private http: HttpClient,
+  ) { }
+
+  loginUser(loginInfo: ILogin): String {
+    // return this.http.post<ILoginToken>(`${environment.backend}/auth/signin`, loginInfo)
+    //   .map(token => {
+    //     localStorage.setItem('retroUserToken', token.token)
+    //   });
+    return '';
+  }
 
 }
