3fc5fa625938b7e3bf03cae90dd64d03d5399426
David Bronfen <davidbronfen@gmail.com>
2019-07-17 14:35:05 +0300

    Remove the js server app.

--- _server/api/api.js
+++
@@ -1,7 +0,0 @@
-const router = require('express').Router();
-
-// api router will mount other routers for all our resources.
-router.use('/categories', require('./category/categoryRoutes'));
-router.use('/recipes', require('./recipe/recipeRoutes'));
-
-module.exports = router;
--- _server/index.js
+++
@@ -1,7 +0,0 @@
-const config = require('./config/config');
-const app = require('./server');
-const logger = require('./util/logger');
-
-app.listen(config.port, () => {
-  logger.log([`listening on http://localhost:${config.port}`]);
-});
