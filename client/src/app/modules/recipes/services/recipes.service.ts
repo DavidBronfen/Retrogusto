7a31268b7e13a03209b045bd2b775da31b7b8d18
David Bronfen <davidbronfen@gmail.com>
2019-07-20 18:22:38 +0300

    Recipe service and component fix.

--- client/src/app/modules/recipes/services/recipes.service.ts
+++ client/src/app/modules/recipes/services/recipes.service.ts
@@ -1,28 +1,17 @@
-import {throwError as observableThrowError,  Observable } from 'rxjs';
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpResponse, HttpErrorResponse, HttpHeaders } from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 import { environment } from '../../../../environments/environment';
 
-import { IRecipes } from '../models/recipes';
+import { IRecipesResponse } from '../models/recipes';
 
 @Injectable()
 export class RecipesService {
   readonly envBackend = environment.backend;
   constructor(private _http: HttpClient) { }
 
-  getRecipes (): Observable<IRecipes[]> {
-    return this._http.get<IRecipes[]>(`${this.envBackend}/api/recipes/`)
-      .map(recipes =>
-        recipes.map(recipe =>
-          Object.assign(recipe, { image_path: this.envBackend + recipe.image_path })
-        )
-      )
-      .catch(this.handleError);
-  }
-
-  private handleError(err: HttpErrorResponse) {
-    console.log(err);
-    return observableThrowError(err.error.message || 'server error');
+  getRecipes (): Observable<IRecipesResponse> {
+    return this._http.get<IRecipesResponse>(`${this.envBackend}/recipes/`)
   }
 }
