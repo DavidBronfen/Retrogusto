17ffe28d078b02bbf68ebe3b6b5f664176590927
David Bronfen <davidbronfen@gmail.com>
2019-07-26 10:21:54 +0300

    Add some routing to handle login and register.

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,13 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-import { CategoryComponent } from './components/category/category.component';
 import { CategoriesComponent } from './components/categories/categories.component';
+import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
+import { HomeComponent } from './components/home/home.component';
 
 const routes: Routes = [
-  { path: '', redirectTo: 'categories', pathMatch: 'full' },
-  { path: 'categories', component: CategoriesComponent },
-  { path: 'recipes/:categoryName', loadChildren: () => import('app/modules/recipes/recipes.module').then(m => m.RecipesModule) },
+  { path: '', component: HomeComponent, pathMatch: 'full' },
+  { path: 'login', component: LoginComponent },
+  { path: 'register', component: RegisterComponent },
+  { path: ':me/categories', component: CategoriesComponent },
+  { path: ':me/recipes/:categoryName', loadChildren: () => import('app/modules/recipes/recipes.module').then(m => m.RecipesModule) },
 ];
 
 @NgModule({
