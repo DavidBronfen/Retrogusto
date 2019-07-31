404aeb18378ec659cc8edb3f6097baee54e698af
David Bronfen <davidbronfen@gmail.com>
2019-07-31 13:13:01 +0300

    Added guard infrastructure.

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,17 +1,36 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
+import { AuthGuard } from './guard/auth.guard';
+
 import { CategoriesComponent } from './components/categories/categories.component';
 import { LoginComponent } from './components/login/login.component';
 import { RegisterComponent } from './components/register/register.component';
 import { HomeComponent } from './components/home/home.component';
 
 const routes: Routes = [
-  { path: '', component: HomeComponent, pathMatch: 'full' },
+  {
+    path: '',
+    component: HomeComponent,
+    canActivate: [AuthGuard],
+    canActivateChild: [AuthGuard],
+    children: [
+      {path: '', redirectTo: 'categories', pathMatch: 'full'},
+      {
+        path: 'categories',
+        component: CategoriesComponent,
+      },
+      {
+        path: ':categoryName/recipes',
+        loadChildren: () => import('app/modules/recipes/recipes.module')
+          .then(m => m.RecipesModule),
+        canActivate: [AuthGuard]
+      },
+    ]
+  },
   { path: 'login', component: LoginComponent },
   { path: 'register', component: RegisterComponent },
-  { path: ':me/categories', component: CategoriesComponent },
-  { path: ':me/recipes/:categoryName', loadChildren: () => import('app/modules/recipes/recipes.module').then(m => m.RecipesModule) },
+  { path: '**', redirectTo: '', pathMatch: 'full' },
 ];
 
 @NgModule({
