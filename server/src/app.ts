f9db56d922efe79f5a7855bc6b3a8632f3da2b61
David Bronfen <davidbronfen@gmail.com>
2019-08-02 23:31:37 +0300

    Remove unused code.

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,7 +1,6 @@
 import * as bodyParser from "body-parser";
 import * as cors from "cors";
 import * as express from "express";
-import * as path from "path";
 
 import * as categoryRoutes from "./api/category/categoryRoutes";
 import * as recipeRoutes from "./api/recipe/recipeRoutes";
